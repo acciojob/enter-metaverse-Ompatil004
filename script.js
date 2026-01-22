cced885b4ceb322f5ad336d64aa97f4e33f674b0
Om Patil <155781720+Ompatil004@users.noreply.github.com>
2026-01-22 13:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let text=document.getElementById("status");
+let btn=document.getElementById("enterBtn");
+
+function enterMetaverse(){
+	text.innerHTML="<h1>Entered Metaverse</h1>";
+}
